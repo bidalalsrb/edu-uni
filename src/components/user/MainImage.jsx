2ea269c9825acbd5471e52cf17266937b76fadfa
bidalalsrb <bidal.mingyu@gmail.com>
2025-03-23 00:49:10 +0900

    tailwind 3v

--- src/components/user/MainImage.jsx
+++ src/components/user/MainImage.jsx
@@ -5,11 +5,11 @@ import logo from "/public/per.png";
 function MainImage() {
     return (
         <div className="bg-gray-100 p-4">
-            <h2 className="text-center text-3xl font-semibold text-gray-800 mb-6">상담 리스트</h2>
+            <h2 className="text-center text-3xl font-semibold text-gray-800 mb-6">메인 포스터 자리</h2>
             <img
                 src={logo}
                 alt="Main"
-                className="w-full object-cover rounded-md h- "
+                className="w-full max-h-[500px] object-contain rounded-md"
             />
             <div className="ob"/>
         </div>
